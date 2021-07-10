c2b3391cefd887cd9fe4613a50147ab65dfaf70e
sagar saha <sagar@sagars-MacBook-Pro.local>
2021-07-11 03:20:21 +0530

    change db

--- demo/settings.py
+++ demo/settings.py
@@ -87,13 +87,13 @@ DATABASES = {
         'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432',
-        'CONN_MAX_AGE': 500
+        # 'CONN_MAX_AGE': 500
     }
 }
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
 
 
